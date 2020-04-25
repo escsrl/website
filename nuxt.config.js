082335203280f4a126bcfae88e3f748a25272404
Alessandro Bellanda <a.bellanda@esconsulting.it>
2020-04-25 22:21:44 +0200

    fixed scrollBehavior

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,39 +110,6 @@ export default {
      */
     extend(config, ctx) {}
   },
-  router: {
-    async scrollBehavior(to, from, savedPosition) {
-      if (savedPosition) {
-        return savedPosition
-      }
-
-      const findEl = (hash, x = 0) => {
-        return (
-          document.querySelector(hash) ||
-          new Promise((resolve) => {
-            if (x > 50) {
-              return resolve(document.querySelector('#app'))
-            }
-            setTimeout(() => {
-              resolve(findEl(hash, ++x || 1))
-            }, 50)
-          })
-        )
-      }
-
-      if (to.hash) {
-        const el = await findEl(to.hash)
-        if ('scrollBehavior' in document.documentElement.style) {
-          return window.scrollTo({ top: el.offsetTop })
-        } else {
-          return window.scrollTo(0, el.offsetTop)
-        }
-      }
-
-      return { x: 0, y: 0 }
-    }
-  },
-  // https://toor.co/blog/nuxtjs-smooth-scrolling-with-hash-links/
   sitemap: {
     hostname: 'https://esconsulting.it',
     gzip: true
