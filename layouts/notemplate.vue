a2cce43f013e0eeadc31faf38f8d43fb55ecb306
Mattia Vitali <vitali.mattia@gmail.com>
2020-10-28 11:28:14 +0100

    feat: page covid19

---
+++ layouts/notemplate.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+body {
+  font-family: 'Lato', sans-serif;
+}
+</style>
+<script>
+export default {
+  components: {},
+  middleware: 'resetServices'
+}
+</script>
---
+++ pages/covid19.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div
+      class="row pt-md-5"
+      style="text-align: center; cursor: pointer; margin: 0 auto;"
+      @click="clickLink"
+    >
+      <div class="col-12 mb-4">
+        <b-img src="/img/covid/covid.jpg" class="text-center" />
+      </div>
+      <div
+        class="col-12 text-center fontSizeTitle fontLatoBlack fontPinkText text-uppercase mt-4"
+      >
+        Esc Statistiche dei contagi Covid-19 aggiornate in tempo reale
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Covid19',
+  components: {},
+  layout: 'notemplate',
+  computed: {},
+  methods: {
+    clickLink() {
+      window.open('https://covid19.esconsulting.it', '_blank')
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {})
+  }
+}
+</script>
+
+<style scoped></style>
