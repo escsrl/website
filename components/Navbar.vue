b74851d5056dea360a91cb4dabfdc5d6aa8a04bc
Mattia Vitali <vitali.mattia@gmail.com>
2021-10-19 12:47:36 +0200

    feat: aggiunta la pagina della navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -24,6 +24,9 @@
           <b-dropdown-item class="voce_sottomenu" to="/bi"
             >BUSINESS INTELLIGENCE SELF SERVICE</b-dropdown-item
           >
+          <b-dropdown-item class="voce_sottomenu" to="/escyber"
+            >ESCYBER</b-dropdown-item
+          >
           <b-dropdown-item
             class="voce_sottomenu"
             href="https://bit.ly/esconsulting-campagna-joobto"
---
+++ pages/escyber.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <hero-section
+      top-classes="businessUno backgroundSchermataCinque"
+      slide-class="slideBusinessWA"
+      image="/img/new_service/webapplication_header.png"
+      alt-text="Interoperabilità"
+      text="ESCYBER VALUTA' LE TUE CONOSCENZE SULLA SYBER SECURITY"
+    />
+
+    <div class="row pt-md-5 pt-sm-3 pt-3 pt-integrazioni ">
+      <div
+        class="col-12 text-center fontSizeTitle fontLatoBlack fontPinkText text-uppercase"
+      >
+        Escyber
+      </div>
+    </div>
+    <b-container
+      class="text-center pt-md-5 pb-md-5 pt-sm-3 pb-sm-3 pt-3 pb-3 mb-xl-5"
+    >
+      <b-row
+        class="col-9 text-center mx-auto fontSizeText fontLatoRegular fontGrayText"
+      >
+        <p>
+          Una soluzione a impatto zero sui percorsi aziendali, con l'obiettivo
+          di alzare il livello di attenzione e il grado di consapevolezza e
+          competenza nei dipendenti.<br />
+          EsCyber inoltre ti fornirà statistiche e indicatori di rischio sempre
+          aggiornati.
+        </p>
+      </b-row>
+
+      <b-row align-v="center">
+        <b-col>
+          <!-- Download broschure -->
+          <div
+            class="click_link game_download"
+            style="margin-top: 5%;"
+            @click="goToLink('https://game.esconsulting.it/quiz/cyberSecurity')"
+          >
+            <b-img src="/img/game/cyber_security.jpg" />
+            <div class="testo fontGrayText">
+              Gioca ora!
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import { sync } from 'vuex-pathify'
+import HeroSection from '../components/HeroSection'
+
+export default {
+  name: 'EsCyber',
+  components: { HeroSection },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.keyToOmit = 1
+    })
+  },
+  layout: 'services',
+  data() {
+    return {}
+  },
+  computed: {
+    keyToOmit: sync('service/keyToOmit')
+  },
+  methods: {
+    goToLink(link) {
+      window.open(link, '_blank')
+    }
+  }
+}
+</script>
+
+<style scoped></style>
